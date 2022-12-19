cf08b2d6ba7594c5c82ba09aa45770716083e50d
Utku-Iray <51332027+Utku-Iray@users.noreply.github.com>
2022-12-20 01:26:05 +0300

    Php kurulumları

---
+++ php/footer.php
@@ -0,0 +1,108 @@
+<footer class="footer footer-1">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-6 col-md-6 col-lg-2">
+                    <div class="footer-widget widget-links">
+                        <div class="footer-widget-title">
+                            <h5>company</h5>
+                        </div>
+                        <div class="widget-content">
+                            <ul>
+                                <li><a href="page-about.html">About Us</a></li>
+                                <li><a href="page-team.html">Meet Our Team</a></li>
+                                <li><a href="blog-grid.html">News & Media</a></li>
+                                <li><a href="projects-standard.html">Our Projects</a></li>
+                                <li><a href="page-contact.html">contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!--  End .col-lg-2-->
+                <div class="col-12 col-sm-6 col-md-6 col-lg-2">
+                    <div class="footer-widget widget-links">
+                        <div class="footer-widget-title">
+                            <h5>Services</h5>
+                        </div>
+                        <div class="widget-content">
+                            <ul>
+                                <li><a href="services-turbines.html">Solar Panels</a></li>
+                                <li><a href="services-turbines.html">Wind Turbines</a></li>
+                                <li><a href="services-turbines.html">Hydropower Plants</a></li>
+                                <li><a href="services-turbines.html">Fossil Resources</a></li>
+                                <li><a href="services-turbines.html">Battery Materials</a></li>
+                                <li><a href="services-turbines.html">biofuels</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!--  End .col-lg-2-->
+                <div class="col-12 col-sm-6 col-md-6 col-lg-5">
+                    <div class="footer-widget widget-links widget-icon">
+                        <div class="footer-widget-title">
+                            <h5>support</h5>
+                        </div>
+                        <div class="widget-content">
+                            <ul>
+                                <li><a href="page-contact.html">Terms & Conditions</a></li>
+                                <li><a href="page-contact.html">Shipping Policy</a></li>
+                                <li><a href="page-contact.html">Delivery Tips</a></li>
+                                <li><a href="page-contact.html">Returns</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!--  End .col-lg-5-->
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="footer-widget widget-contact">
+                        <div class="widget-content">
+                            <ul>
+                                <li class="phone">+2<a href="tel:+01061245741"> 01061245741</a></li>
+                                <li class="email">Email: <a href="mailto:info@energia.com">info@energia.com</a></li>
+                                <li class="address">
+                                    <p>2307 Beverley Rd Brooklyn, New York 11226 United States.</p>
+                                </li>
+                                <li class="directions"><a href="page-contact.html"><i class="energia-location-Icon"></i>get directions</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!--  End .col-lg-3-->
+            </div>
+            <!-- End .row-->
+        </div>
+        <!--  End .container-->
+    </div>
+    <!--  End .footer-top-->
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="footer-copyright">
+                        <div class="copyright"><span>&copy; 2021 energia by <a href="https://1.envato.market/kP9BV"> Zytheme.com</a>. all rights reserved. </span>
+                            <ul class="list-unstyled social-icons">
+                                <li> <a class="share-facebook" href="javascript:void(0)"><i class="energia-facebook"></i>facebook </a></li>
+                                <li> <a class="share-twitter" href="javascript:void(0)"><i class="energia-twitter"></i>twitter</a></li>
+                                <li> <a class="share-youtube" href="javascript:void(0)"><i class="energia-youtube"></i>youtube</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--  End .row-->
+    </div>
+    <!--  End .footer-bottom-->
+</footer>
+<!--
+      ============================
+      BackToTop #1
+      ============================
+      -->
+<div class="back-top" id="back-to-top" data-hover=""><i class="energia-arrow-up"></i></div>
+</div>
+<!--  Footer Scripts==
+    -->
+<script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+<script src="assets/js/vendor.js"></script>
+<script src="assets/js/functions.js"></script>
